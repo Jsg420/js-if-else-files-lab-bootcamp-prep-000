8c8827935d18db122839dbe6d3de32f4d2958da6
Johnny Gonzales <jsg420@gmail.com>
2017-12-28 20:20:16 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -2,14 +2,9 @@ var index = require("./index.js")
 
 if (index.name === Joe) {
     console.log("The name is correct")
-} else {
-    console.log("Expected: Joe, Received: "+index.name)
-}
-
+} 
 
 
 if (index.height === 74) {
     console.log("The height is correct")
-} else {
-    console.log("Expected: 74, Received: "+index.height)
-}
+} 
